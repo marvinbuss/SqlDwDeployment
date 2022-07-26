218f8072365bcb53f0ca2e2150c7d03dc53ec41f
Marvin Buss <marvin.buss@gmail.com>
2022-07-26 18:51:39 +0200

    Update table

--- code/MySqlProject/IrisSchema/Tables/Sales.sql
+++ code/MySqlProject/IrisSchema/Tables/Sales.sql
@@ -1,10 +1,12 @@
 CREATE TABLE [IrisSchema].[Sales]
 (
-  [Id] INT NOT NULL
+  [Id] INT NOT NULL,
+  [Variety] [varchar](30) NOT NULL,
+  [Price] [decimal](5,2) NOT NULL
 )
 WITH
 (
-    DISTRIBUTION = HASH ( [Id] ),
+    DISTRIBUTION = HASH ( [Variety] ),
     CLUSTERED COLUMNSTORE INDEX
 )
 GO
