8f601158f0c4d517647567a3f1bfaf845a17a60c
Marvin Buss <marvin.buss@gmail.com>
2022-07-28 10:00:17 +0200

    Remove column type change

--- code/MySqlProject/IrisSchema/Tables/Iris.sql
+++ code/MySqlProject/IrisSchema/Tables/Iris.sql
@@ -2,7 +2,7 @@ CREATE TABLE [IrisSchema].[Iris] (
     [NewSepalLength] DECIMAL (18) NOT NULL,
     [SepalWidth]     DECIMAL (18) NOT NULL,
     [PetalLength]    DECIMAL (18) NOT NULL,
-    [PetalWidth]     FLOAT (53)   NOT NULL,
+    [PetalWidth]     DECIMAL (18) NOT NULL,
     [Variety]        VARCHAR (30) NOT NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = HASH([Variety]));
