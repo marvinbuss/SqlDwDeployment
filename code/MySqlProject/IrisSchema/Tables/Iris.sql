911dfa76b314a3ff08729b1db19062e2b4e95bfb
Marvin Buss <marvin.buss@gmail.com>
2022-07-28 09:26:42 +0200

    Update columns

--- code/MySqlProject/IrisSchema/Tables/Iris.sql
+++ code/MySqlProject/IrisSchema/Tables/Iris.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [IrisSchema].[Iris]
 (
-    [SepalLength] [decimal](18,0) NOT NULL,
+    [NewSepalLength] [decimal](18,0) NOT NULL,
     [SepalWidth] [decimal](18,0) NOT NULL,
     [PetalLength] [decimal](18,0) NOT NULL,
     [PetalWidth] [float](53) NOT NULL,
-    [NewVariety] [varchar](30) NOT NULL
+    [Variety] [varchar](30) NOT NULL
 )
 WITH
 (
