df54671127c6fc11861069cec0827f4e663cef47
Marvin Buss <marvin.buss@gmail.com>
2022-07-28 11:01:32 +0200

    Update column name

--- code/MySqlProject/IrisSchema/Tables/Iris.sql
+++ code/MySqlProject/IrisSchema/Tables/Iris.sql
@@ -1,9 +1,14 @@
-CREATE TABLE [IrisSchema].[Iris] (
-    [NewSepalLength] DECIMAL (18) NOT NULL,
-    [SepalWidth]     DECIMAL (18) NOT NULL,
-    [PetalLength]    DECIMAL (18) NOT NULL,
-    [PetalWidth]     DECIMAL (18) NOT NULL,
-    [Variety]        VARCHAR (30) NOT NULL
+CREATE TABLE [IrisSchema].[Iris]
+(
+    [NewSepalLength] [decimal](18,0) NOT NULL,
+    [SepalWidth] [decimal](18,0) NOT NULL,
+    [PetalLength] [decimal](18,0) NOT NULL,
+    [PetalWidth] [decimal](18,0) NOT NULL,
+    [Variety] [varchar](30) NOT NULL
 )
-WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = HASH([Variety]));
+WITH
+(
+    DISTRIBUTION = HASH ( [Variety] ),
+    CLUSTERED COLUMNSTORE INDEX
+)
 GO
