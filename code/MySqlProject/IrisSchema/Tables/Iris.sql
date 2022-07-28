7edbc5c3777a53099c676580e356427a0207d930
Marvin Buss <marvin.buss@gmail.com>
2022-07-28 09:22:37 +0200

    Update column name and data type

--- code/MySqlProject/IrisSchema/Tables/Iris.sql
+++ code/MySqlProject/IrisSchema/Tables/Iris.sql
@@ -3,8 +3,8 @@ CREATE TABLE [IrisSchema].[Iris]
     [SepalLength] [decimal](18,0) NOT NULL,
     [SepalWidth] [decimal](18,0) NOT NULL,
     [PetalLength] [decimal](18,0) NOT NULL,
-    [PetalWidth] [decimal](18,0) NOT NULL,
-    [Variety] [varchar](30) NOT NULL
+    [PetalWidth] [float](53) NOT NULL,
+    [NewVariety] [varchar](30) NOT NULL
 )
 WITH
 (
