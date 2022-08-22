3b6de3c9004c39963c96579e1c58fc900bf1e710
Marvin Buss <marvin.buss@gmail.com>
2022-08-22 15:34:06 +0200

    Updated table definition

--- code/MyDotNetSqlProject/MyDotNetSqlProject/IrisSchema/Tables/Iris.sql
+++ code/MyDotNetSqlProject/MyDotNetSqlProject/IrisSchema/Tables/Iris.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [IrisSchema].[Iris]
 (
-    [SepalLength] DECIMAL NOT NULL,
-    [SepalWidth]  DECIMAL (18) NOT NULL,
-    [PetalLength] DECIMAL (18) NOT NULL,
-    [PetalWidth]  DECIMAL (18) NOT NULL,
-    [Variety]     VARCHAR (30) NOT NULL
+    [SepalLength]    DECIMAL(18) NOT NULL,
+    [SepalWidth]     DECIMAL (18) NOT NULL,
+    [PetalLength]    DECIMAL (18) NOT NULL,
+    [PetalWidth]     DECIMAL (18) NOT NULL,
+    [Variety]        VARCHAR (40) NOT NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = HASH([Variety]));
