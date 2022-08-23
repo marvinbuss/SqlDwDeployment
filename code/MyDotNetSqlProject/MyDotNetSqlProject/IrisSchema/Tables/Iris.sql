336defb3f39a55d5ac4f15585c6396d7939aec16
Marvin Buss <marvin.buss@gmail.com>
2022-08-23 18:06:36 +0200

    Update Column Type Length

--- code/MyDotNetSqlProject/MyDotNetSqlProject/IrisSchema/Tables/Iris.sql
+++ code/MyDotNetSqlProject/MyDotNetSqlProject/IrisSchema/Tables/Iris.sql
@@ -4,6 +4,6 @@
     [SepalWidth]     DECIMAL (18) NOT NULL,
     [PetalLength]    DECIMAL (18) NOT NULL,
     [PetalWidth]     DECIMAL (18) NOT NULL,
-    [Variety]        VARCHAR (40) NOT NULL
+    [Variety]        VARCHAR (50) NOT NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = HASH([Variety]));
